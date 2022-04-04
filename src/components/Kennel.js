48e35fe68c74afe0636c3a59f354a7b38e7270cf
JavontaeH <hardeman.javontae@gmail.com>
2022-04-04 14:13:24 -0500

    initial commit

---
+++ src/components/Kennel.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Kennel.css";
+import { AnimalCard } from "./animal/AnimalCard";
+import { PropsAndState } from "./PropsAndState";
+
+export const Kennel = () => (
+  <>
+    <h2>Nashville Kennels</h2>
+    <small>Loving care when you're not there.</small>
+    <address>
+      <div>Visit Us at the Nashville North Location</div>
+      <div>500 Puppy Way</div>
+    </address>
+    <PropsAndState yourName="Jae" />
+    <h2>Animals</h2>
+    <article className="animals">
+      <AnimalCard />
+      <AnimalCard />
+      <AnimalCard />
+    </article>
+  </>
+);
