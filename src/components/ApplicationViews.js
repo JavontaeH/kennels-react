4f2ed5fa8c4a4f6ccf52b4d15f2208099697545f
JavontaeH <hardeman.javontae@gmail.com>
2022-04-07 13:34:30 -0500

    added better auth using private outlet

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import { Home } from "./home/Home";
 import { AnimalList } from "./animal/AnimalList";
 import { EmployeeList } from "./employee/EmployeeList";
@@ -12,8 +12,8 @@ import { Register } from "./auth/Register";
 import { Login } from "./auth/Login";
 
 export const ApplicationViews = ({ isAuthenticated, setIsAuthenticated }) => {
-  const PrivateRoute = ({ children }) => {
-    return isAuthenticated ? children : <Navigate to="/login" />;
+  const PrivateOutlet = () => {
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
   };
 
   const setAuthUser = (user) => {
@@ -31,77 +31,20 @@ export const ApplicationViews = ({ isAuthenticated, setIsAuthenticated }) => {
         />
         <Route exact path="/register" element={<Register />} />
         {/* Render the home page */}
-        <Route
-          exact
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/" element={<PrivateOutlet />}>
+          <Route exact path="" element={<Home />} />
 
-        {/* Render the list when http://localhost:3000/(list) is chosen */}
+          {/* Render the list when http://localhost:3000/(list) is chosen */}
 
-        <Route
-          exact
-          path="/animals"
-          element={
-            <PrivateRoute>
-              <AnimalList />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/animals/:animalId"
-          element={
-            <PrivateRoute>
-              <AnimalDetail />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/animals/create"
-          element={
-            <PrivateRoute>
-              <AnimalForm />
-            </PrivateRoute>
-          }
-        />
+          <Route exact path="animals" element={<AnimalList />} />
+          <Route path="animals/:animalId" element={<AnimalDetail />} />
+          <Route path="animals/create" element={<AnimalForm />} />
 
-        <Route
-          exact
-          path="/locations"
-          element={
-            <PrivateRoute>
-              <LocationList />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/locations/:locationId"
-          element={
-            <PrivateRoute>
-              <LocationDetail />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/customers"
-          element={
-            <PrivateRoute>
-              <CustomerList />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/employees"
-          element={
-            <PrivateRoute>
-              <EmployeeList />
-            </PrivateRoute>
-          }
-        />
+          <Route exact path="locations" element={<LocationList />} />
+          <Route path="locations/:locationId" element={<LocationDetail />} />
+          <Route path="customers" element={<CustomerList />} />
+          <Route path="employees" element={<EmployeeList />} />
+        </Route>
       </Routes>
     </>
   );
