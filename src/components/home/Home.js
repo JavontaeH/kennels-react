cb1b7379f889bc71deabf2ab9eefbdf5a3c6f966
JavontaeH <hardeman.javontae@gmail.com>
2022-04-08 12:00:45 -0500

    added animal spotlight

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PropsAndState } from "../propsAndState/PropsAndState";
 import { Checkbox } from "../propsAndState/Checkbox";
+import { getRandomId } from "../../modules/DataManager";
+import { AnimalSpotlight } from "../animal/AnimalSpotlight";
 
 const getUsername = () => {
   return JSON.parse(sessionStorage.getItem("kennel_customer"));
 };
 
-export const Home = () => (
-  <>
-    <h2>Nashville Kennels</h2>
-    <small>Loving care when you're not there.</small>
+export const Home = () => {
+  const [spotlightId, setSpotlightId] = useState(0);
 
-    <address>
-      <div>Visit Us at the Nashville North Location</div>
-      <div>500 Puppy Way</div>
-    </address>
-    <PropsAndState yourName={getUsername()?.name} />
-    <Checkbox />
-  </>
-);
+  const refreshSpotlightAnimal = () => {
+    getRandomId().then(setSpotlightId);
+  };
+
+  useEffect(() => {
+    refreshSpotlightAnimal();
+  }, []);
+
+  return (
+    <>
+      <h2>Nashville Kennels</h2>
+      <small>Loving care when you're not there.</small>
+
+      <address>
+        <div>Visit Us at the Nashville North Location</div>
+        <div>500 Puppy Way</div>
+      </address>
+      <button onClick={refreshSpotlightAnimal}>Reload &#x27f3;</button>
+      {spotlightId && <AnimalSpotlight animalId={spotlightId} />}
+      <PropsAndState yourName={getUsername()?.name} />
+      <Checkbox />
+    </>
+  );
+};
